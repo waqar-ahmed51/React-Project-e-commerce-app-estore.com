501b5e7c4a7bdf4c201ba3b2868fff34419f1e4d
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-06-13 18:38:46 +0500

    Slider Added

--- e-commerce-project/src/components/Announcement.jsx
+++ e-commerce-project/src/components/Announcement.jsx
@@ -3,8 +3,13 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 30px;
-  background-color: #6028ff;
+  background-color: #2168fb;
   color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
 `;
 const Announcement = () => {
   return <Container>Announcements will be here soon!</Container>;
--- e-commerce-project/src/components/Navbar.jsx
+++ e-commerce-project/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Badge } from "@material-ui/core";
-import { MailOutline, Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
