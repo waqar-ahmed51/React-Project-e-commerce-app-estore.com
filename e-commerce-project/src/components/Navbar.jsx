9f59566f78edf6bacc9cc96504ea097662929abf
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-06-13 14:17:12 +0500

    Navbar styling

---
+++ e-commerce-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  backgroud-color: black;
+`;
+
+const Navbar = () => {
+  return <Container>Navbar is working!</Container>;
+};
+
+export default Navbar;
