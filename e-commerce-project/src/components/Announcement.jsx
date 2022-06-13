44052c2a9352ffc81f9188559f9c3a7b453f45bc
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-06-13 21:30:36 +0500

    Slider Click Handler in Progress

--- e-commerce-project/src/components/Announcement.jsx
+++ e-commerce-project/src/components/Announcement.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 30px;
-  background-color: #2168fb;
+  background-color: #000000;
   color: white;
   display: flex;
   align-items: center;
--- e-commerce-project/src/components/Slider.jsx
+++ e-commerce-project/src/components/Slider.jsx
@@ -8,10 +8,13 @@ const Container = styled.div`
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transform: translateX(-100vw);
 `;
 
 const Arrow = styled.div`
@@ -37,6 +40,7 @@ const Slide = styled.div`
   width: 100vw;
   height: 100vh;
   align-items: center;
+  /* background-color: #${(props) => props.bg}; */
 `;
 
 const ImageContainer = styled.div`
@@ -45,27 +49,81 @@ const ImageContainer = styled.div`
 `;
 const InfoContainer = styled.div`
   flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  margin: 0px 20px;
+  font-size: 70px;
+`;
+
+const Desc = styled.p`
+  margin: 30px 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  margin: 0px 20px;
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Image = styled.img`
-  height: 90vh;
-  width: 100vw;
+  height: 70%;
 `;
 
 const Slider = () => {
+  const handledClick = (direction) => {};
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={handledClick("left")}>
         <KeyboardArrowLeftOutlined />
       </Arrow>
       <Wrapper>
-        <Slide>
+        <Slide bg="b30808">
+          <InfoContainer>
+            <Title>BUY TECH NOW</Title>
+            <Desc>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+              porro exercitationem,perferendis a iste.
+            </Desc>
+            <Button>BUY NOW</Button>
+          </InfoContainer>
           <ImageContainer>
-            <Image src="https://images.pexels.com/photos/3985062/pexels-photo-3985062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+            <Image src="https://www.brostudiocz.com/wp-content/gallery/product-photography-on-white/hive-xl-front.jpg" />
+          </ImageContainer>
+        </Slide>
+        <Slide bg="072122">
+          <InfoContainer>
+            <Title>BUY TECH NOW2</Title>
+            <Desc>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+              porro exercitationem,perferendis a iste.
+            </Desc>
+            <Button>BUY NOW</Button>
+          </InfoContainer>
+          <ImageContainer>
+            <Image src="https://www.brostudiocz.com/wp-content/gallery/product-photography-on-white/hive-xl-front.jpg" />
+          </ImageContainer>
+        </Slide>
+        <Slide bg="e410c8">
+          <InfoContainer>
+            <Title>BUY TECH NOW3</Title>
+            <Desc>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+              porro exercitationem,perferendis a iste.
+            </Desc>
+            <Button>BUY NOW</Button>
+          </InfoContainer>
+          <ImageContainer>
+            <Image src="https://www.brostudiocz.com/wp-content/gallery/product-photography-on-white/hive-xl-front.jpg" />
           </ImageContainer>
         </Slide>
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={handledClick("right")}>
         <KeyboardArrowRightOutlined />
       </Arrow>
     </Container>
