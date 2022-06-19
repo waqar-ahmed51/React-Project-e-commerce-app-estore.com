151d2703ff9330b196216fb61959078988ff7ab2
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-06-19 17:32:32 +0500

    Update App.js

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -9,7 +9,7 @@ import Cart from "./pages/Cart";
 const App = () => {
   return <Home />;
   // return <ProductList />;
-  // return <ProductView />;
+  return <ProductView />;
   // return <Register />;
   // return <SignIn />;
   return <Cart />;
