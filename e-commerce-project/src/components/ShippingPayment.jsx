10e36e99d2dbd84b36d1ebc4efbe3d717bae29cc
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-06-20 20:55:41 +0500

    Update ShippingPayment.jsx

--- e-commerce-project/src/components/ShippingPayment.jsx
+++ e-commerce-project/src/components/ShippingPayment.jsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import { Checkbox } from "@material-ui/core";
 
 const Wrapper = styled.div`
   margin: 20px;
-  height: 60vh;
+  height: 600px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -19,18 +18,26 @@ const Title = styled.h1`
   flex-direction: column;
 `;
 
-const RegisterFields = styled.div`
+const ShippingPaymentContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  width: 35vw;
 `;
 const InputField = styled.input`
   border: 1px solid black;
   padding: 10px;
   margin-bottom: 10px;
-  width: 30vw;
+  width: 100%;
 `;
+const InputFieldAddress = styled.textarea`
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+`;
+
 const SubmitButton = styled.button`
   border: none;
   border: 1px solid black;
@@ -48,34 +55,96 @@ const SubmitButton = styled.button`
   }
 `;
 
-const Agreement = styled.div`
+const SelectFiled = styled.select`
+  padding: 10px;
+  border: 1px solid black;
+  margin-bottom: 10px;
+  width: 104%;
+`;
+const Option = styled.option``;
+
+const PurchaseSummary = styled.div`
+  border: 1px solid black;
+  margin: 10px 0px;
+  background-color: #edeaea;
+  padding: 10px;
+  padding-bottom: 10px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const PriceContianer = styled.div`
+  display: flex;
+  /* justify-content: center;
+  align-content: center; */
+`;
+const PriceTitle = styled.p`
+  background-color: black;
+  border-bottom: 0.2px solid white;
+  color: white;
+  display: flex;
+  padding: 5px;
+  justify-content: flex-end;
+  align-items: center;
+  flex: 1;
+  font-size: 15px;
+`;
+const PriceDetail = styled.p`
   display: flex;
   align-items: center;
-`;
-const AgreementText = styled.p`
+  justify-content: flex-end;
+  padding: 5px;
+  flex: 3;
+  background-color: white;
+  border-bottom: 0.2px solid black;
   font-size: 15px;
-  color: #757575;
   &:hover {
-    color: black;
+    background-color: #e4e3e3;
+    transition: all 0.2s ease;
   }
+`;
+const SummaryTitle = styled.h3`
+  text-align: center;
+  margin: 5px 0px;
+  padding: 5px 0px;
 `;
 
 const ShippingPayment = () => {
   return (
     <Wrapper>
-      <RegisterFields>
-        <Title>CREATE YOUR ACCOUNT</Title>
-        <InputField placeholder="First Name"></InputField>
-        <InputField placeholder="Last Name"></InputField>
-        <InputField placeholder="Email"></InputField>
-        <InputField placeholder="Password" type="password"></InputField>
-        <InputField placeholder="Confirm Password" type="password"></InputField>
-        <Agreement>
-          <Checkbox color="black" />
-          <AgreementText>AGREE TO OUR TERMS AND POLICIES</AgreementText>
-        </Agreement>
-        <SubmitButton>REGISTER</SubmitButton>
-      </RegisterFields>
+      <ShippingPaymentContainer>
+        <Title>SHIPPING AND PAYMENT DETAILS</Title>
+        <PurchaseSummary>
+          <SummaryTitle>Summary</SummaryTitle>
+          <PriceContianer>
+            <PriceTitle>Subtotal </PriceTitle>
+            <PriceDetail>3,20,000 PKR</PriceDetail>
+          </PriceContianer>
+          <PriceContianer>
+            <PriceTitle>Delivery </PriceTitle>
+            <PriceDetail>1000 PKR</PriceDetail>
+          </PriceContianer>
+          <PriceContianer>
+            <PriceTitle style={{ fontSize: "30px" }}>Total </PriceTitle>
+            <PriceDetail style={{ fontSize: "30px" }}>3,20,100 PKR</PriceDetail>
+          </PriceContianer>
+        </PurchaseSummary>
+        <InputField placeholder="Full Name"></InputField>
+        <InputField placeholder="Contact" type="number"></InputField>
+        <SelectFiled>
+          <Option disabled selected>
+            City
+          </Option>
+          <Option>Karachi</Option>
+          <Option>Lahore</Option>
+          <Option>Islamabad</Option>
+          <Option>Multan</Option>
+        </SelectFiled>
+        <InputFieldAddress placeholder="Address"></InputFieldAddress>
+        <SubmitButton>PLACE ORDER</SubmitButton>
+      </ShippingPaymentContainer>
     </Wrapper>
   );
 };
