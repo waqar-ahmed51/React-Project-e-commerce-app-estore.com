aadbac5fcf4a1ed9d4440a3fbcae12b083f076ec
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-06-21 16:00:15 +0500

    React Router Issue

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 // import ProductView from "./pages/ProductView";
 // import Register from "./pages/Register";
 // import SignIn from "./pages/SignIn";
 // import Cart from "./pages/Cart";
-// import Checkout from "./pages/Checkout";
+import Checkout from "./pages/Checkout";
 
 
 const App = () => {
-  return <Home />;
+  // return <Home />;
   // return <ProductList />;
   // return <ProductView />;
   // return <Register />;
   // return <SignIn />;
   // return <Cart />;
+<<<<<<< HEAD
   // return <Checkout />;
+=======
+  return <Checkout/>;
+>>>>>>> parent of 784996f (Update App.js)
 };
 
 export default App;
