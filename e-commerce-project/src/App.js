f2267d3961f9c9247698fd377aa7e79ab248d6d7
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-06-21 17:02:44 +0500

    Routers Implemented

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
 import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import ProductView from "./pages/ProductView";
-// import Register from "./pages/Register";
-// import SignIn from "./pages/SignIn";
-// import Cart from "./pages/Cart";
-// import Checkout from "./pages/Checkout";
+import ProductList from "./pages/ProductList";
+import ProductView from "./pages/ProductView";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 const App = () => {
-  return <Home />;
+
+  return (
+    <BrowserRouter>
+    <div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/productview" element={<ProductView />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+  
   // return <ProductList />;
   // return <ProductView />;
   // return <Register />;
