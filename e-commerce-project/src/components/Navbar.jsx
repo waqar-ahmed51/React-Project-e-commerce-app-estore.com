64f84e8914cf67cb4011cf2352a78172227d93a5
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-07-04 17:12:18 +0500

    Fixed Navbar

--- e-commerce-project/src/components/Navbar.jsx
+++ e-commerce-project/src/components/Navbar.jsx
@@ -7,6 +7,18 @@ import { Link } from "react-router-dom";
 const Container = styled.div`
   height: 60px;
   border-bottom: 1px solid #b7b7b7;
+  background-color: white;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 100;
+`;
+
+const SpaceDiv = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: black;
 `;
 
 const Wrapper = styled.div`
@@ -62,40 +74,44 @@ const MenuItem = styled.div`
 
 const Navbar = ({ ItemsInCart }) => {
   return (
-    <Container>
-      <Wrapper>
-        <Left>
-          <Link to="/" className="CustomRouterLink">
-            <Logo>estore.</Logo>
-          </Link>
-        </Left>
-        <Center>
-          <SearchContainer>
-            <Input />
-            <Search style={{ color: "black", fontSize: 20 }} />
-          </SearchContainer>
-        </Center>
-        <Right>
-          <MenuItem to="/register">
-            <Link to="/register" className="CustomRouterLink">
-              REGISTER
+    <div>
+      <Container>
+        <Wrapper>
+          <Left>
+            <Link to="/" className="CustomRouterLink">
+              <Logo>estore.</Logo>
             </Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/signin" className="CustomRouterLink">
-              SIGN IN
-            </Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/cart" className="CustomRouterLink">
-              <Badge badgeContent={ItemsInCart} color="primary">
-                <ShoppingCartOutlined />
-              </Badge>
-            </Link>
-          </MenuItem>
-        </Right>
-      </Wrapper>
-    </Container>
+          </Left>
+          <Center>
+            <SearchContainer>
+              <Input />
+              <Search style={{ color: "black", fontSize: 20 }} />
+            </SearchContainer>
+          </Center>
+          <Right>
+            <MenuItem to="/register">
+              <Link to="/register" className="CustomRouterLink">
+                REGISTER
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/signin" className="CustomRouterLink">
+                SIGN IN
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/cart" className="CustomRouterLink">
+                <Badge badgeContent={ItemsInCart} color="primary">
+                  <ShoppingCartOutlined />
+                </Badge>
+              </Link>
+            </MenuItem>
+          </Right>
+        </Wrapper>
+      </Container>
+      {/* Dummy div for creating space behing the navbar so all content pushed to 60px below the navbar */}
+      <SpaceDiv />
+    </div>
   );
 };
 
