bf319e241ff9d3562511583da4dcf54ef66671c9
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-07-29 17:06:05 +0500

    Update App.js

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -163,8 +163,10 @@ class App extends Component {
         <PopMessage id="popMessage" >{this.state.MessageText}</PopMessage>
       <Routes>
           <Route path="/" element={<Home addItemCart={this.handleAddItemCart}/>} />
+
           {/* Same link for github pages */}
           <Route path="/React-Project-e-commerce-app-estore.com/" element={<Home addItemCart={this.handleAddItemCart}/>} />
+          
           <Route path="/productlist/:category" element={<ProductList addItemCart={this.handleAddItemCart}/>} />
           <Route path="/productview/:id" element={<ProductView addItemCart={this.handleAddItemCart}/>} />
           <Route path="/register" element={<Register />} />
