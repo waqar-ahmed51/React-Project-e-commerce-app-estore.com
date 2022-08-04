7d102d96c636c4fc21f0c1f1fc59c86a9592956d
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-08-04 20:15:47 +0500

    Update App.js

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -1,6 +1,6 @@
 // Clone of the React State-Props based estore e-commerce app - Redux Implementing (Making state centralized)
 import React, { Component } from 'react';
-import {ToastContainer,toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./App.css";
 import Home from "./pages/Home";
@@ -14,8 +14,8 @@ import DataAPI from './pages/DataAPI';
 import AddProductToAPI from './pages/AddProductToAPI';
 import RestfulAPIData from './pages/RestfulAPIData';
 import ErrorPage from "./pages/ErrorPage";
-import { CartItemsData,totaPriceOfCartItemsData } from './data';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { CartItemsData, totaPriceOfCartItemsData } from './data';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SearchResult from './pages/SearchResult';
@@ -23,38 +23,38 @@ import SearchResult from './pages/SearchResult';
 class App extends Component {
   state = {
     //Preventing duplication product to cart
-    itemsIDsStorage:[],
-     cartItemsTotalNum : 0,
-     //fetching cart items from the CartItemsData fro data.js and putting in state.
+    itemsIDsStorage: [],
+    cartItemsTotalNum: 0,
+    //fetching cart items from the CartItemsData fro data.js and putting in state.
     cartItems: CartItemsData,
     //Data in state for the checkout page. Total Cost of the items in cart to checkout page.
     totalPriceCartItems: totaPriceOfCartItemsData,
-   } 
+  }
 
-   handleAddItemCart = (item) => {
+  handleAddItemCart = (item) => {
     // console.log("Test which item is passed to cart :", item.id);
     // console.log("Ids storgae :", this.state.itemsIDsStorage);
-    let itemsIDsStorage=this.state.itemsIDsStorage;
-    if(!itemsIDsStorage.includes(item.id)){
+    let itemsIDsStorage = this.state.itemsIDsStorage;
+    if (!itemsIDsStorage.includes(item.id)) {
       CartItemsData.push(item);
-      this.setState({cartItems:CartItemsData});
+      this.setState({ cartItems: CartItemsData });
       //Preventing duplication product to be added in cart - based on state itemsIDsStorage (keeps ids for the products)
       itemsIDsStorage.push(item.id);
-      this.setState({itemsIDsStorage})
+      this.setState({ itemsIDsStorage })
       let cartItemsTotalNum = this.state.cartItemsTotalNum;
       cartItemsTotalNum++;
       this.setState({ cartItemsTotalNum });
       //Showing Toast Message - when product added to cart 
       toast.success('Product added to Cart', {
-        position: toast.POSITION.BOTTOM_LEFT
+        position: toast.POSITION.TOP_LEFT
       });
-    }else{
+    } else {
       //Showing Toast Up Message - when user tries to add produt again.
       toast.error('Product already in Cart', {
-        position: toast.POSITION.BOTTOM_LEFT
+        position: toast.POSITION.TOP_LEFT
       });
     }
-  }; 
+  };
 
   //Product Quantity and total price of the particular product
   onhandleAddQuantity = (id) => {
@@ -66,86 +66,87 @@ class App extends Component {
       CartItemsData[id].price);
     this.setState({ priceQuantityShow });
     // Updating navbar cart total quantity
-    let cartItemsTotalNum=this.state.cartItemsTotalNum;
+    let cartItemsTotalNum = this.state.cartItemsTotalNum;
     cartItemsTotalNum++;
     // Preventing Cart total count to be negative
-    if(cartItemsTotalNum<0){
-      cartItemsTotalNum=0;
+    if (cartItemsTotalNum < 0) {
+      cartItemsTotalNum = 0;
     }
-    this.setState({cartItemsTotalNum})
+    this.setState({ cartItemsTotalNum })
   };
 
   //Decrease quantity of the product in the cart
   onhandleRemoveQuantity = (id) => {
     // Controlling quantity to go below 1
-    if(CartItemsData[id].quantity>1){
-    const decrement = --CartItemsData[id].quantity;
-    this.setState({ decrement });
+    if (CartItemsData[id].quantity > 1) {
+      const decrement = --CartItemsData[id].quantity;
+      this.setState({ decrement });
       //Updating the price per quantity in the state for the product
-    let priceQuantityShow = (CartItemsData[id].priceQuantity -=
-      CartItemsData[id].price);
-    this.setState({ priceQuantityShow });
-    // Updating navbar cart total quantity
-    let cartItemsTotalNum=this.state.cartItemsTotalNum;
-    cartItemsTotalNum--;
-    // Preventing Cart total count to be negative
-    if(cartItemsTotalNum<0){
-      cartItemsTotalNum=0;
+      let priceQuantityShow = (CartItemsData[id].priceQuantity -=
+        CartItemsData[id].price);
+      this.setState({ priceQuantityShow });
+      // Updating navbar cart total quantity
+      let cartItemsTotalNum = this.state.cartItemsTotalNum;
+      cartItemsTotalNum--;
+      // Preventing Cart total count to be negative
+      if (cartItemsTotalNum < 0) {
+        cartItemsTotalNum = 0;
+      }
+      this.setState({ cartItemsTotalNum })
     }
-    this.setState({cartItemsTotalNum})
-  }  };
+  };
 
   // Deleteing products in the cart
-  onhandleDeleteProduct=(itemid,  itemquantity)=>{
+  onhandleDeleteProduct = (itemid, itemquantity) => {
     var deleteItemsInCart = this.state.cartItems;
-    deleteItemsInCart=this.state.cartItems.filter(function(deleteItemsInCart){
+    deleteItemsInCart = this.state.cartItems.filter(function (deleteItemsInCart) {
       return deleteItemsInCart.id !== itemid;
     });
     // Deleting all ojects in CartItemsData in data.js and pushing the filtered objects/items to CartItemsData data.js
-    const start=0;
-    const end=CartItemsData.length;
-    CartItemsData.splice(start,end);
-    deleteItemsInCart.filter(function(DataCartItemPush){
+    const start = 0;
+    const end = CartItemsData.length;
+    CartItemsData.splice(start, end);
+    deleteItemsInCart.filter(function (DataCartItemPush) {
       CartItemsData.push(DataCartItemPush);
       return DataCartItemPush.id !== "nothing";
     });
-    this.setState({cartItems: deleteItemsInCart});
+    this.setState({ cartItems: deleteItemsInCart });
     // Updating navbar cart total quantity - fetching from CartItem.jsx as parameter
-    let cartItemsTotalNum=this.state.cartItemsTotalNum;
-    cartItemsTotalNum=cartItemsTotalNum-itemquantity;
+    let cartItemsTotalNum = this.state.cartItemsTotalNum;
+    cartItemsTotalNum = cartItemsTotalNum - itemquantity;
     // Preventing Cart total count to be negative
-    if(cartItemsTotalNum<0){
-      cartItemsTotalNum=0;
+    if (cartItemsTotalNum < 0) {
+      cartItemsTotalNum = 0;
     }
-    this.setState({cartItemsTotalNum})
+    this.setState({ cartItemsTotalNum })
     //Deleting IDs from itemsIDsStorage Array in state so User can add product again to cart.
-    let itemsIDsStorage=this.state.itemsIDsStorage;
-    let index=itemsIDsStorage.indexOf(itemid);
-    itemsIDsStorage[index]=99999;
-    this.setState({itemsIDsStorage});
+    let itemsIDsStorage = this.state.itemsIDsStorage;
+    let index = itemsIDsStorage.indexOf(itemid);
+    itemsIDsStorage[index] = 99999;
+    this.setState({ itemsIDsStorage });
   };
 
   render() {
     return (
       <Router>
-        <Navbar ItemsInCart={this.state.cartItemsTotalNum}/>
-      <Routes>
-          <Route path="/" element={<Home addItemCart={this.handleAddItemCart}/>} />
+        <Navbar ItemsInCart={this.state.cartItemsTotalNum} />
+        <Routes>
+          <Route path="/" element={<Home addItemCart={this.handleAddItemCart} />} />
           {/* Same link for github pages */}
-          <Route path="/React-Project-e-commerce-app-estore.com/" element={<Home addItemCart={this.handleAddItemCart}/>} />
-          <Route path="/productlist/:category" element={<ProductList addItemCart={this.handleAddItemCart}/>} />
-          <Route path="/productview/:id" element={<ProductView addItemCart={this.handleAddItemCart}/>} />
+          <Route path="/React-Project-e-commerce-app-estore.com/" element={<Home addItemCart={this.handleAddItemCart} />} />
+          <Route path="/productlist/:category" element={<ProductList addItemCart={this.handleAddItemCart} />} />
+          <Route path="/productview/:id" element={<ProductView addItemCart={this.handleAddItemCart} />} />
           <Route path="/register" element={<Register />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/cart" element={<Cart 
-          AddQuantity={this.onhandleAddQuantity}
-          RemoveQuantity={this.onhandleRemoveQuantity}
-          DeleteProduct={this.onhandleDeleteProduct}
-          cartItems={this.state.cartItems}
+          <Route path="/cart" element={<Cart
+            AddQuantity={this.onhandleAddQuantity}
+            RemoveQuantity={this.onhandleRemoveQuantity}
+            DeleteProduct={this.onhandleDeleteProduct}
+            cartItems={this.state.cartItems}
           />} />
-          <Route path="/checkout" element={<Checkout 
-          totalCartItems={this.state.cartItemsTotalNum}
-          totalPriceCartItems={this.state.totalPriceCartItems}
+          <Route path="/checkout" element={<Checkout
+            totalCartItems={this.state.cartItemsTotalNum}
+            totalPriceCartItems={this.state.totalPriceCartItems}
           />} />
           <Route path="/dataapi" element={<DataAPI />} />
           <Route path="/addproducttoapi" element={<AddProductToAPI />} />
@@ -153,11 +154,11 @@ class App extends Component {
           <Route path="/searchresult/:keyword" element={<SearchResult />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
-        <Footer/>
-        <ToastContainer/>
+        <Footer />
+        <ToastContainer />
       </Router>
     );
   }
 }
- 
+
 export default App;
